a0ca92a04e69dbd976ac549e527609a0637706b0
tuananh19dc <tuananh19dc@gmail.com>
2022-04-11 17:33:42 +0700

    tao relationship trong model

--- app/stask.php
+++ app/stask.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class stask extends Model
 {
-    //
+
+    public function users(){
+        return $this->belongsToMany('App\User','stask_user','stask_id','user_id');
+    }
 }
